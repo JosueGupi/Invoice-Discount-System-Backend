c5d95a342984f6a0bec26462a0db5c6449e8ec7a
JosueGupi <74017438+JosueGupi@users.noreply.github.com>
2023-09-11 14:53:35 -0600

    Check request struct

--- routes/Users.js
+++ routes/Users.js
@@ -20,7 +20,7 @@ app.post("/login", function (req, res) {
 
 app.post("/evalEmail", function (req, res) {
     const email = req.body.email;
-
+console.log(req);
     connection.query(`SELECT  idUser FROM users WHERE Email = ${user};`,
         function (err, result) {
             console.log(result);
