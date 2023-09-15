afd89d855450dbd23f6a70197613748dc175ba96
JosueGupi <74017438+JosueGupi@users.noreply.github.com>
2023-09-15 13:13:10 -0600

    CRUD banks

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const express = require("express")
 const app = express()
 const Users = require('./routes/Users')
 const Accounts = require('./routes/Accounts')
+const Banks = require('./routes/Banks')
 app.use(express.json())
 app.use(cors())
 
 app.use('/users',Users)
 app.use('/accounts',Accounts)
+app.use('/banks',Banks)
 
 
 app.listen(process.env.PORT || 3001,()=>{
---
+++ routes/Banks.js
@@ -0,0 +1,73 @@
+const app = require("express").Router();
+const { response } = require("express");
+const connection = require("../mysql");
+
+app.get("/getBanks", function (req, res) {
+
+
+    connection.query(`SELECT idBank, Name FROM banks;`,
+        function (err, result) {
+
+            if (err) {
+                res.json(err);
+                throw err;
+            }
+            else {
+                res.json(result)
+            }
+        }
+    );
+});
+app.post("/createBank", function (req, res) {
+    const name = req.body.name;
+
+    connection.query(`INSERT INTO banks (Name) VALUES  ('${name}')`,
+        function (err, result) {
+
+            if (err) {
+                res.json(err);
+                throw err;
+            }
+            else {
+                res.json(result)
+            }
+        }
+    );
+});
+app.post("/updateBank", function (req, res) {
+    const name = req.body.name,
+        idBank = req.body.idBank;
+
+
+    connection.query(`UPDATE banks SET Name = ${name} WHERE idBank = ${idBank}`,
+        function (err, result) {
+
+            if (err) {
+                res.json(err);
+                throw err;
+            }
+            else {
+                res.json(result)
+            }
+        }
+    );
+});
+
+app.post("/deleteBank", function (req, res) {
+    const idBank = req.body.idBank;
+
+
+    connection.query(`DELETE FROM Banks WHERE idBank = ${idBank}`,
+        function (err, result) {
+
+            if (err) {
+                res.json(err);
+                throw error;
+            }
+            else {
+                res.json(result)
+            }
+        }
+    );
+});
+module.exports = app;
