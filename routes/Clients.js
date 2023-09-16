363a7e0b18937a54bbf143e99c8e5492057a43e4
lalicas <hilaryc.cr@estudiantec.cr>
2023-09-15 20:55:50 -0600

    CRUD clients

--- routes/Clients.js
+++ routes/Clients.js
@@ -5,7 +5,64 @@ const connection = require("../mysql");
 app.get("/getClients", function (req, res) {
 
 
-    connection.query(`SELECT idClient, Name FROM clients;`,
+    connection.query(`SELECT idClient, Name, Email, IdentificationCard FROM clients;`,
+        function (err, result) {
+
+            if (err) {
+                res.json(err);
+                throw err;
+            }
+            else {
+                res.json(result)
+            }
+        }
+    );
+});
+
+app.post("/createClient", function (req, res) {
+    const name = req.body.name;
+    const email = req.body.email;
+    const idenCard = req.body.idenCard;
+
+    connection.query(`INSERT INTO banks (Name, Email, IdentificationCard) VALUES  ('${name}', '${email}', '${idenCard}')`,
+        function (err, result) {
+
+            if (err) {
+                res.json(err);
+                throw err;
+            }
+            else {
+                res.json(result)
+            }
+        }
+    );
+});
+app.post("/updateClient", function (req, res) {
+    const name = req.body.name;
+    const email = req.body.email;
+    const idenCard = req.body.idenCard,
+            idClient = req.body.idClient;
+
+
+    connection.query(`UPDATE banks SET Name = '${name}', Email = '${email}', IdentificationCard = '${idenCard}'  WHERE idClient = ${idClient}`,
+        function (err, result) {
+
+            if (err) {
+                res.json(err);
+                throw err;
+            }
+            else {
+                res.json(result)
+            }
+        }
+    );
+});
+
+app.post("/deleteBank", function (req, res) {
+    const idClient = req.body.idClient;
+
+
+    connection.query(`DELETE FROM Banks WHERE idBank = ${idClient}`,
         function (err, result) {
 
             if (err) {
