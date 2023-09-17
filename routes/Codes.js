bc6fa2622ac1c7a4b245fc8943d8c30285738f28
Valeria Chinchilla <vchinchilla02@hotmail.com>
2023-09-17 14:12:43 -0600

    Update Codes

--- routes/Codes.js
+++ routes/Codes.js
@@ -3,7 +3,29 @@ const { response } = require("express");
 const connection = require("../mysql");
 
 app.get("/getCodes", function (req, res) {
-    connection.query(`SELECT Code, Description, clients.Name FROM accountingcodes INNER JOIN clients ON accountingcodes.idClient = clients.idClient;`,
+    connection.query(`SELECT idAccountingCodes, Code, Description, clients.Name FROM accountingcodes INNER JOIN clients ON accountingcodes.idClient = clients.idClient;`,
+        function (err, result) {
+
+            if (err) {
+                res.json(err);
+                throw err;
+            }
+            else {
+                res.json(result);
+            }
+        }
+    );
+});
+
+app.post("/updateCode", function (req, res) {
+    console.log(req.body);
+    const idClient = Number(req.body.idClient),
+        code = Number(req.body.number),
+        description = Number(req.body.description),
+        idAccountingCodes = Number(req.body.idAccountingCodes);
+
+
+    connection.query(`UPDATE accountingcodes SET idClient = ${idClient}, Code = '${code}', Description = ${description} WHERE idAccountingCodes = ${idAccountingCodes}`,
         function (err, result) {
 
             if (err) {
