1397550fa7493dba36df158c2963e1cef292a7a4
Valeria Chinchilla <vchinchilla02@hotmail.com>
2023-11-15 20:10:45 -0600

    delete console logs

--- routes/Operations.js
+++ routes/Operations.js
@@ -129,9 +129,7 @@ app.get("/calendarOperations", function (req, res) {
 });
 
 app.get("/operationDetail", function (req, res) {
-    console.log(req.body);
     const idOperation = Number(req.body.idOperation);
-    console.log(idOperation);
     connection.query(`CALL SP_GetOperationDetail(${idOperation});`,
         function (err, result) {
 
