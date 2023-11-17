7e83132cd34a769147883ce172cd02b121365328
Valeria Chinchilla <vchinchilla02@hotmail.com>
2023-11-16 18:10:39 -0600

    add matrix email

--- routes/Users.js
+++ routes/Users.js
@@ -1,6 +1,11 @@
 const app = require("express").Router();
 const { response } = require("express");
 const connection = require("../mysql");
+const bodyParser = require('body-parser');
+const sgMail = require('@sendgrid/mail');
+
+app.use(bodyParser.json());
+sgMail.setApiKey('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');
 
 app.post("/login", function (req, res) {
     const password = req.body.password,
@@ -85,7 +90,6 @@ app.post("/changePassword", function (req, res) {
 });
 app.get("/getUsers", function (req, res) {
 
-
     connection.query(`CALL SP_GetUsers();`,
         function (err, result) {
 
@@ -104,6 +108,32 @@ app.post("/createUser", function (req, res) {
         email = req.body.email,
         password = req.body.password
 
+    // enviar correo
+    const data = generateRandomMatrix(5);
+    console.log(data)
+    const html = generateHTMLTable(data);
+    console.log(html)
+
+    const msg = {
+        to: email,
+        from: 'sistemaInverEllens@gmail.com',
+        subject: 'Matriz de Seguridad',
+        html: html,
+    };
+
+    sgMail
+        .send(msg)
+        .then(() => {
+            console.log('Correo enviado con éxito');
+            res.send('Correo enviado con éxito');
+        })
+        .catch((error) => {
+            console.error(error.toString());
+            res.status(500).send('Error al enviar correo');
+        });
+
+
+
     connection.query(`CALL SP_CreateUser('${name}', '${email}', '${password}');`,
         function (err, result) {
 
@@ -116,7 +146,50 @@ app.post("/createUser", function (req, res) {
             }
         }
     );
+
+    // agregar a la base la matriz
+
 });
+
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateRandomLetter() {
+    const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    return letters.charAt(Math.floor(Math.random() * letters.length));
+}
+
+function generateRandomMatrix(size) {
+    const matrix = {};
+    for (let i = 0; i < size; i++) {
+        const letter = generateRandomLetter();
+        if (!matrix[letter]) {
+            matrix[letter] = [];
+        }
+        for (let j = 0; j < 5; j++) {
+            matrix[letter].push(generateRandomNumber(1, 25));
+        }
+    }
+    return matrix;
+}
+function generateHTMLTable(data) {
+    let html = '<table><tr>';
+    for (const key in data) {
+        html += `<th>${key}</th>`;
+    }
+    html += '</tr>';
+    for (let i = 0; i < data[Object.keys(data)[0]].length; i++) {
+        html += '<tr>';
+        for (const key in data) {
+            html += `<td>${data[key][i]}</td>`;
+        }
+        html += '</tr>';
+    }
+    html += '</table>';
+    return html;
+}
+
 app.post("/updateUser", function (req, res) {
     const name = req.body.name,
         email = req.body.email,
